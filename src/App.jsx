2673acf12f8a4e3181211516150f92d6c4b95a43
mayur-driod <settymayurk@gmail.com>
2025-03-10 12:47:03 +0530

    assignment 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import ItemList from "./components/ItemList";
 
 // use the following link to get the data
@@ -6,10 +7,27 @@ const API_URI = `https://${import.meta.env.VITE_API_URI}/doors`;
 
 function App() {
   // Get the existing item from the server
-  // const [items, setItems] = useState(null);
+  const [items, setItems] = useState(null);
   // pass the item to UpdateItem as a prop
 
-  return <ItemList />;
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URI);
+        if (!response.ok) {
+          console.log("Failed to fetch data");
+        }
+        const data = await response.json();
+        setItems(data);
+      } catch (error) {
+        console.log("Error fetching items:", error);
+      }
+    };
+    
+    fetchItems();
+  }, []);
+
+  return <ItemList key={items.id} items={items} />;
 }
 
 export default App;
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,7 +2,15 @@ const Item = ({ item }) => {
 
     // Render a single item
     // Add a Delete and Edit button
-    return null;
+    return(
+        <>
+            <div>
+                <h1>{item.id}</h1>
+                <p>{item.name}</p>
+                <p>{item.status}</p>
+            </div>
+        </>
+    )
 };
 
 export default Item;
